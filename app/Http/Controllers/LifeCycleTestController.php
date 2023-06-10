b40f51b2d542e56a369dd14c64c0964942d05f73
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-06-10 21:34:24 +0900

    ライフサイクルテスト

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PHPUnit\Framework\MockObject\ClassIsFinalException;
+
+class LifeCycleTestController extends Controller
+{
+  public function showServiceContainerTest()
+  {
+    app()->bind('lifeCycleTest', function(){
+      return 'ライフサイクルテスト';
+    });
+
+    $test = app()->make('lifeCycleTest');
+
+    //サービスコンテナなしのパターン
+    $message = new Message();
+    $sample = new Sample($message);
+    $sample->run();
+    dd($test, app());
+  }
+}
+
+class Sample {
+  public $message;
+  public function __construct(Message $message) {
+    $this->message = $message;
+  }
+  public function run() {
+    $this->message->send();
+  }
+}
+
+class Message {
+  public function send(){
+    echo('メッセージ表示');
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\componentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 
 
 /*
@@ -17,20 +18,21 @@ use App\Http\Controllers\componentTestController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/component-test1', [componentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [componentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [lifeCycleTestController::class, 'showServiceContainerTest']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
